7fef826a074e7a4da3aaf20eca4825fb1949971f
Zachary Klinger <zklinger@zmac.local>
2017-07-11 10:33:30 -0700

    controller(facebook): Fix a Promise not returned error

--- src/controllers/facebook.js
+++ src/controllers/facebook.js
@@ -3,6 +3,56 @@ import jwt from 'jsonwebtoken';
 const User = require('../models/User');
 
 const facebookController = {
+
+  loginCallback: (req, res) => {
+    res.redirect(`${process.env.WEB_APP_URL}/login/return?token=${req.user.token}`);
+  },
+
+  requireAuth: (req, res, next) => {
+    const token = req.get('authorization');
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return res.status(500).send(err);
+
+      User.findOne({ _id: decoded._id })
+        .lean()
+        .exec()
+        .then(user => {
+          if (user) {
+            req.user = user;
+            req.token = token;
+            next();
+            return null;
+          } else {
+            res.status(401).send('No user found');
+          }
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    });
+  },
+
+  me: (req, res) => {
+    const authUser = req.user;
+    const token = req.token;
+
+    User.findOne({ _id: authUser._id })
+      .lean()
+      .then(user => {
+        // If a user DOES exist, return token and whitelisted user info
+        if (user) {
+          res.status(200).json({
+            token,
+            displayName: user.facebook.displayName
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  },
+
   webhook: (req, res) => {
     if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
       console.log('Verified webhook');
@@ -12,54 +62,6 @@ const facebookController = {
       console.error('Verification failed. The tokens do not match.');
       res.sendStatus(403);
     }
-  },
-
-  loginCallback: (req, res) => {
-    res.redirect(`${process.env.WEB_APP_URL}/login/return?token=${req.user.token}`);
-  },
-
-  me: (req, res) => {
-    const token = req.get('authorization');
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return res.status(500).send(err);
-      // console.log('decoded:\n', decoded);
-      User.findOne({ _id: decoded._id })
-        .lean()
-        .then(user => {
-          // If a user DOES exist, return token and whitelisted user info
-          if (user) {
-            res.status(200).json({
-              token,
-              displayName: user.facebook.displayName
-            });
-          }
-        })
-        .catch(err => {
-          res.status(500).send(err);
-        });
-    });
-  },
-
-  requireAuth: (req, res, next) => {
-    const token = req.get('authorization');
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return res.status(500).send(err);
-      User.findOne({ _id: decoded._id })
-        .lean()
-        .then(user => {
-          if (user) {
-            req.user = user;
-            next();
-          } else {
-            res.status(401).send('No user found');
-          }
-        })
-        .catch(err => {
-          res.status(500).send(err);
-        });
-    });
   }
 };
 
